4962f18fcceb665b61211917a74527b23b35542e
Paul De Cunha <pauldecunha7117@gmail.com>
2023-02-12 17:14:56 -0500

    improved formula

--- DNA/geneFinder.java
+++ DNA/geneFinder.java
@@ -3,36 +3,7 @@ import java.io.*;
 
 
 public class GeneFinderAndTester {
-    public String findGeneSimple(String dna) {
-        String result = 'invalid sequence';
-        int startIndex = dna.indexOf('ATG');
-        int endIndex = dna.indexOf('TAA', startIndex + 3); 
-		if(startIndex > -1 || endIndex > -1) {
-				if ((startIndex - endIndex) % 3 == 0) {
-// 			return dna.substring(start, stop+3);
-// 		}
-        result = dna.substring(startIndex, endIndex + 3);
-		}
-		return result;
-    }
-}
-
-
-public String findGene (String dna) {
-	dna = dna.toLowercase();
-	int startIndex = dna.indexOf('dna');
-	int currentIndex = dna.indexOf('TAA', startIndex + 3);
-	while (currentIndex != -1 ) {
-		if((currentIndex - startIndex) % 3 == 0) {
-			return dna.substring(startIndex, currentIndex + 3);
-		}else {
-			currentIndex = dna.indexOf('TAA', currentIndex + 1);
-		}
-	}
-	return ''
-}
-
-public int findStopCodon(String dna, int startIndex, String stopCodon) {
+ public int findStopCodon(String dna, int startIndex, String stopCodon) {
 	int currIndex = dna.indexof(stopCodon, startIndex + 3);
 	while( currIndex != -1 ) {
 		int diff = currIndex - startIndex;
@@ -41,6 +12,8 @@ public int findStopCodon(String dna, int startIndex, String stopCodon) {
 	} else {
 		currIndex = dna.indexOf(stopCodon)
 	}
+}
+return -1;
 }
 
 public String findTheGene (String dna) {
@@ -56,26 +29,43 @@ public String findTheGene (String dna) {
 	int temp = Math.min(taaIndex, tagIndex);
 	int min = Math.min(temp,tgaIndex);
 
+
+	if(taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)) {
+		minIndex = tgaIndex;
+	} else {
+		minIndex = taaIndex;
+	}
+
+	if (minIndex == -1 || (tgaIndex != -1 && tagIndex < min)) {
+		minIndex = tagIndex;
+	}
+
 	if( min == dna.length()){
 		return ''
 	}
 	return dna.substring(startIndex, min + 3 );
 }
-
+public void testStopCodon () {
+	String dna = 'tffffgssfvsfdvsdTAAdtgrdgdfggdgggrTAAuu';
+	int test = findStopCodon(dna, 0, 'TAA');
+	if (test != 16 ) System.out.println('error');
+	test = findStopCodon(dna, 16, 'TAA');
+	if (test != 16 ) System.out.println('error after 16');
+}
 public void testGeneFinder(){
     String sample = 'AATGCGTAATATGGT';
     System.out.println(" The DNA strand is " + sample);
-    String gene = geneFinder(sample);
+    String gene = findTheGene(sample);
     System.out.println("The gene is " + gene);
 
     sample = 'AATGCTAGGGTAATATGGT';
     System.out.println(" The DNA strand is " + sample);
-    gene = geneFinder(sample);
+    gene = findTheGene(sample);
     System.out.println("The gene is " + gene);
 
     sample = 'ATCTATGCTTCGGCTGVTVTAATATGGT';
     System.out.println(" The DNA strand is " + sample);
-    gene = geneFinder(sample);
+    gene = findTheGene(sample);
     System.out.println("The gene is " + gene);
 }
 }
