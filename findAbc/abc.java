367336aae03e1a114dd9770a4c014b9123549034
Paul De Cunha <pauldecunha7117@gmail.com>
2023-02-16 12:12:55 -0500

    abc finder

---
+++ findAbc/abc.java
@@ -0,0 +1,22 @@
+
+
+public class abc {
+public void findAbc(String input){
+       int index = input.indexOf("abc");
+       while (true){
+           if (index == -1 || index >= input.length() - 3){
+               break;
+           }
+              System.out.println("index " + index);
+           String found = input.substring(index+1, index+4);
+           System.out.println(found);
+         index = input.indexOf("abc",index+3);  
+           System.out.println("index after updating " + index);
+       }
+   }
+
+   public void test(){
+       //findAbc("abcdkfjsksioehgjfhsdjfhksdfhuwabcabcajfieowj");
+       findAbc("abcabcabcabca");
+   }
+}
