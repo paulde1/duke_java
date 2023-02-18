e2d21ac83e9d91e643a9748f34dc24ffb95ad684
Paul De Cunha <pauldecunha7117@gmail.com>
2023-02-17 21:12:53 -0500

    CSV parser

---
+++ csv/CSV.java
@@ -0,0 +1,13 @@
+import edu.duke. *;
+import org.apache.commons.csv.*;
+
+public class CSV {
+    public void readFood() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.get CSVParser();
+        for(CSVRecord record : parser) {
+            System.out.print(record.get("name"));
+             System.out.println(record.get("Favorite food"));
+        };
+    };
+};
