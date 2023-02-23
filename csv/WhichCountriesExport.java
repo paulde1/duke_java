651624b01349c89e9a3714d0a307f9e0ea836de7
Paul De Cunha <pauldecunha7117@gmail.com>
2023-02-22 22:20:07 -0500

    CSV parser - country

---
+++ csv/WhichCountriesExport.java
@@ -0,0 +1,17 @@
+import edu.duke. *;
+import org.apache.commons.csv.*;
+
+public class WhichCountriesExport {
+    public void listExporters(CSVParser parser, String exportsOfInterests){
+        //for each row in the CSV
+            //check the exports column contains exports
+            //if true print country from row 
+            for(CSVRecord record : parser){
+                String export = record.get('Exports');
+                if(export.contains(exportsOfInterests)) {
+                    String country = record.get('Country');
+                    System.out.println(country)
+                }
+            }
+    }
+}
