ba7f1928d805be2a8943e86c37987c0f1a435b69
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-09 14:47:41 -0500

    webpage parse

---
+++ classA/webpage/WebpageParse.java
@@ -0,0 +1,17 @@
+package classA.webpage;
+import edu.duke.*;
+
+public class WebpageParse {
+    public void readWebpage() {
+        URLResource file = new  URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        for (String item : file.words()) {
+            String itemLower = item.toLowerCase();
+            int pos = itemLower.indexOf("youtube.com");
+            if (pos != -1) {
+                int beg = item.lastIndexOf("\"",pos); //find the specified string backwards
+                int end = item.indexOf("\"", pos+1); //finds the specified string forwards
+                System.out.println(item.substring(beg+1,end));
+            }
+       	}
+    }
+}
