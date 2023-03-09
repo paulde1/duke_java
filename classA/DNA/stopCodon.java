ec6204932bd0f417faaf3c0c8ab9417581defc94
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-09 14:52:34 -0500

    update stop codon

--- classA/DNA/stopCodon.java
+++ classA/DNA/stopCodon.java
@@ -1,4 +1,5 @@
 package classA.DNA;
+import edu.duke.*;
 
 public class stopCodon {
     public int findStopCodon(String dna, int startCodon, String stopCodon) {
@@ -15,6 +16,7 @@ public class stopCodon {
     }
     
     public String findGene(String dna, int where) {
+        dna = dna.toUpperCase();
         int startIndex = dna.indexOf("ATG", where);
         if(startIndex == -1) {
             return "";
@@ -29,56 +31,81 @@ public class stopCodon {
         return dna.substring(startIndex, minIndex + 3);
     }
     
-    public void printAllGenes(String dna) {
+    public StorageResource getAllGenes(String dna) {
         int startIndex = 0;
+        StorageResource geneList = new StorageResource();
+        
         while(true) {
-            String gene = findGene(dna, startIndex);
-            if(gene.isEmpty()) {
+            String currentGene = findGene(dna, startIndex);
+            if(currentGene.isEmpty()) {
                 break;
             }
-            System.out.println(gene);
-            startIndex = dna.indexOf(gene, startIndex) + gene.length();
+            //System.out.println(currentGene);
+            geneList.add(currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex + 1) + currentGene.length();
         }
+        return geneList;
     }
     
-    public int countGenes(String dna) {
-        int count = 0;
-        int startIndex = 0;
+    public double cgRatio(String dna) {
+        int countC = 0;
+        int countG = 0;
+        int cIndex = dna.indexOf("C");
+        int gIndex = dna.indexOf("G");
         while(true) {
-            String gene = findGene(dna, startIndex);
-            if(gene.isEmpty()) {
+            if(cIndex == -1 && gIndex == -1) {
                 break;
+            } else {
+                if(cIndex != -1) {
+                    countC += 1;
+                    cIndex = dna.indexOf("C", cIndex + 1);
+                }
+                if(gIndex != -1) {
+                    countG +=1;
+                    gIndex = dna.indexOf("G", gIndex + 1);
+                }
             }
-            int num = 4;
-            System.out.println(num % 2 == num % 2);
+        }
+        return ((double) countC + countG) / dna.length();
+    }
+    
+    public int countCTG(String dna) {
+        int startIndex = dna.indexOf("CTG");
+        int count = 0;
+        while(startIndex != -1) {
             count++;
-            startIndex = dna.indexOf(gene, startIndex) + gene.length();
+            startIndex = dna.indexOf("CTG", startIndex + 1);
         }
         return count;
     }
     
-    public void testCountGenes() {
-        String dna = "ATGTAAGATGCCCTAGT";
-        System.out.println("DNA is: " + dna);
-        System.out.println("Genes found: " + countGenes(dna));
-        printAllGenes(dna);
+    public void processGenes(StorageResource sr) {
+        String longest = "";
+        double ratio = 0.35d;
+        int countcg = 1;
+        int count = 1;
+        for(String genes : sr.data()) {
+            if(genes.length() > 60) {
+                System.out.println("Gene is: " + genes);
+                System.out.println("Number of strings: " + count);
+                count++;
+            }
+            if(cgRatio(genes) > ratio) {
+                System.out.println("Gene with ratio > 0.35: " + genes);
+                System.out.println("Ratio is: " + cgRatio(genes) + " - count: "+ countcg);
+                countcg++;
+            }
+            if(genes.length() > longest.length()) {
+                longest = genes;
+            }
+        }
+        
+        System.out.println("Longest Gene is: " + longest);
     }
     
-    public void test() {
-        printAllGenes("AATGGCTTGAAATGATTTAGATGTCGTAAATGTGG"); //ATGGCTTGA, ATGATTTAG, ATGTCGTAA
-    }
-    
-    public void testFindStopCodon() {
-        String dna = "ATGTCGTGA"; //TGA index = 6
-        System.out.println(findStopCodon(dna, 0, "TGA"));
-        dna = "ATGTCGTGTAA"; //invalid. should return dna length = 11
-        System.out.println(findStopCodon(dna, 0, "TAA"));
-        dna = "ATGTCGTGCTAA"; //TAA index = 9
-        System.out.println(findStopCodon(dna, 0, "TAA"));
-    }
-    
-    public void testFindGene() {
-        String dna = "ATGTCGTGA"; //ATGTCGTGA
-        System.out.println(findGene(dna, 0));
+    public void testProcessGenes() {
+        FileResource fr = new FileResource("GRch38dnapart.fa");
+        String dna = fr.asString();
+        processGenes(getAllGenes(dna));
     }
 }
