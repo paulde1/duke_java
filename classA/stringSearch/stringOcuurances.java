9122454653579e9c051e1aaa535d0a13a7f09bb7
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-09 14:45:20 -0500

    string searcher

---
+++ classA/stringSearch/stringOcuurances.java
@@ -0,0 +1,38 @@
+package classA.stringSearch;
+
+public class stringOcuurances {
+     public boolean twoOccurrences(String stringa, String stringb) {
+        int count = 0;
+        int lastIndex = 0;
+        while(lastIndex != -1) {
+            lastIndex = stringb.indexOf(stringa, lastIndex);
+            System.out.println(lastIndex);
+            if(lastIndex != -1) {
+                count++;
+                lastIndex += stringa.length();
+            }
+        }
+        if(count > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void testing() {
+        System.out.println(twoOccurrences("by", "A story by Abby Long"));
+        System.out.println(twoOccurrences("a", "banana"));
+        System.out.println(twoOccurrences("atg", "ctgatgta"));
+        System.out.println(lastPart("an", "banana"));
+        System.out.println(lastPart("zoo", "forest"));
+    }
+    
+    public String lastPart(String stringa, String stringb) {
+        int firstOccurence = stringb.indexOf(stringa);
+        if(firstOccurence == -1) {
+            return stringb;
+        } else {
+            return stringb.substring(firstOccurence + stringa.length());
+        }
+    }
+}
