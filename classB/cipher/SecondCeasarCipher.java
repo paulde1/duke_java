765eaf619a6258ddec62367566dfd03de8d519ca
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-15 17:59:52 -0400

    implementing cracking cypher

--- classB/cipher/SecondCeasarCipher.java
+++ classB/cipher/SecondCeasarCipher.java
@@ -9,14 +9,14 @@ public class SecondCeasarCipher {
 	private int key1;
 	private int key2;
 	
-	public CaesarCipherTwo(int key1, int key2){
+	public String CaesarCipherTwo(int key1, int key2){
 		this.key1 = key1;
 		this.key2 = key2;
 		alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		shiftedAlphabet1 = alphabet.substring(key1)+alphabet.substring(0,key1);
 		shiftedAlphabet2 = alphabet.substring(key2)+alphabet.substring(0,key2);
+		return shiftedAlphabet1;
 	}
-	
 	public String encrypt(String input){
 		
 		StringBuilder encrypted = new StringBuilder(input);
