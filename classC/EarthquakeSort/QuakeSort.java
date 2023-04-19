08f2bc42f3423f0262308d298b44fd1868e8ab96
Paul De Cunha <pauldecunha7117@gmail.com>
2023-04-19 16:02:24 -0400

    find the minimum quake location

--- classC/EarthquakeSort/QuakeSort.java
+++ classC/EarthquakeSort/QuakeSort.java
@@ -2,5 +2,12 @@ package classC.EarthquakeSort;
 import java.utils.*;
 
 public class QuakeSort {
-    public int
+    public int getSmallestMagnitude(ArrayList<QuakeEntry> quake);
+    int minimumIndex = from;
+    for(int i = from +1; i < quakes.size(); i++) {
+        if(quakes.get(i).getMagnitude() < quakes.get(minimumIndex){
+            minimumIndex = i;
+        }
+    }
+    return minimumIndex;
 }
