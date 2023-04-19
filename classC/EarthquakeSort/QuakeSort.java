3350e7d33ff1f4f4f3493c522157bb82133b4260
Paul De Cunha <pauldecunha7117@gmail.com>
2023-04-19 16:29:26 -0400

    sort quake method completed

--- classC/EarthquakeSort/QuakeSort.java
+++ classC/EarthquakeSort/QuakeSort.java
@@ -2,12 +2,23 @@ package classC.EarthquakeSort;
 import java.utils.*;
 
 public class QuakeSort {
-    public int getSmallestMagnitude(ArrayList<QuakeEntry> quake);
+    public int getSmallestMagnitude(ArrayList<QuakeEntry> quakes, int from){
     int minimumIndex = from;
     for(int i = from +1; i < quakes.size(); i++) {
-        if(quakes.get(i).getMagnitude() < quakes.get(minimumIndex){
+        if(quakes.get(i).getMagnitude() < quakes.get(minimumIndex).getMagnitude()){
             minimumIndex = i;
         }
     }
     return minimumIndex;
+ }
+ public void sortByMagnitude(ArrayList<QuakeEntry> in) {
+    for (int i = 0; i < in.size(); i++) {
+        int minimumIndex = getSmallestMagnitude(in, i);
+        QuakeEntry qi = in.get(i);
+        QuakeEntry qmin = in.get(minimumIndex);
+        in.set(i, qmin);
+        in.set(minimumIndex, qi);
+    }
+ }
+ 
 }
